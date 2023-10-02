be25e57130de6ad93ed4a10f5d0cafa4d2dd3e04
tesddev <tesleem.amuda@gmail.com>
2023-10-02 14:06:01 +0100

    Initial Commit

---
+++ SwiftUIChatApp/SwiftUIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppApp.swift
+//  SwiftUIChatApp
+//
+//  Created by GIGL iOS on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
