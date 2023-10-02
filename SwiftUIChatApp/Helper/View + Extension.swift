6b1e8bc7d4862ade2b396fd632b072ab307ed7d0
tesddev <tesleem.amuda@gmail.com>
2023-10-02 14:55:50 +0100

    use UIScene.windows in place of deprecated windows

--- SwiftUIChatApp/Helper/View + Extension.swift
+++ SwiftUIChatApp/Helper/View + Extension.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 extension View {
     func endEditing(_ force: Bool) {
-        UIApplication.shared.windows.forEach { $0.endEditing(force)}
+//        UIApplication.shared.windows.forEach { $0.endEditing(force)}
+        if let keyWindowScene = UIApplication.shared.connectedScenes
+            .first(where: { $0 is UIWindowScene && ($0 as? UIWindowScene)?.activationState == .foregroundActive }) as? UIWindowScene {
+            keyWindowScene.windows.forEach { window in
+                window.endEditing(force)
+            }
+        }
     }
 }
